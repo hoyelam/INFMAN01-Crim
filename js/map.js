3e3e1a6baa721fd28873dbc432fb1ff36972faeb
Tjarda112 <tjardavoorn@hotmail.com>
2016-03-29 13:42:51 +0200

    added categories and calender

--- js/map.js
+++ js/map.js
@@ -1,7 +1,3 @@
-/**
- * Created by tjardavoorn on 17/03/16.
- */
-
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
 
@@ -18,6 +14,18 @@ function initMap() {
         disableDoubleClickZoom: true
     });
 
+
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        map: map,
+        data: [],
+        radius: 20
+    });
+
+    getMarkers(heatmap);
+    //marker.addListener('click', toggleBounce);
+}
+
+function getMarkers(heatmap) {
     $.ajax({
         url: 'json/fietsendiefstaldata.json',
         dataType: 'json',
@@ -25,69 +33,19 @@ function initMap() {
         cache: false,
         success: function (data){
             $(data.fietsendiefstal).each(function( index, value){
-
-                if (index < 200) {
-                        $.getJSON("http://maps.googleapis.com/maps/api/geocode/json?address=" + value.Straat + "," + value.Plaats, function (json) {
-                            var long = json.results[0].geometry.location.lng;
-                            var lat = json.results[0].geometry.location.lat;
-
-                            marker = new google.maps.Marker({
-                                map: map,
-                                draggable: true,
-                                animation: google.maps.Animation.DROP,
-                                position: {lat: lat, lng: long}
-                            });
-                        });
-
+                if (index < 100) {
+                    $.getJSON("http://maps.googleapis.com/maps/api/geocode/json?address=" + value.Straat + "," + value.Plaats, function (json) {
+                        console.log(json);
+                        var long = json.results[0].geometry.location.lng;
+                        var lat = json.results[0].geometry.location.lat;
+                        var latLng = new google.maps.LatLng(lat, long);
+                        heatmap.getData().push(latLng);
+                    });
                 }
-                if(index == 200){
+                if(index == 100){
                     return false
                 }
             });
         }
     });
-    //marker.addListener('click', toggleBounce);
 }
-
-
-
-
-/*
-    $(document).ready(function() {
-        var myItems;
-
-        $.getJSON("json/fietsendiefstaldata.json", function(data) {
-            myItems = data.Fietsendiefstal;
-            console.log(myItems);
-        });
-    });
-
-    $.each(data.Fietsendiefstal, function(key, val) {
-        alert(val.MK);
-        alert(val.Poging);
-    });
-
-/*
-    var jqxhr = $.getJSON("json/fietsendiefstaldata.json", function () {
-            console.log("success");
-        })
-        .done(function () {
-            console.log("second success");
-        })
-        .fail(function () {
-            console.log("error");
-        })
-        .always(function () {
-            console.log("complete");
-        });
-
-// Perform other work here ...
-
-// Set another completion function for the request above
-    jqxhr.complete(function () {
-        console.log("second complete");
-        console.log(jqxhr);
-    });
-}
-
-*/
